ffab5694a1fd0055917273c882ffea1e3291dab1
Lara <76517940+lara-dev92@users.noreply.github.com>
2021-07-25 17:25:57 +0300

    25.07 update
    CSS update

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,14 +7,14 @@ import img3 from '../assets/img/img3.jpg';
 function Home() {
 
   const defaultItems=[
-  {image: img1, name: 'Proiect1', category: 'HTML-CSS'}, 
-  {image: img2, name: 'Proiect2', category: 'Javascript'}, 
-  {image: img3, name: 'Proiect3', category: 'Javascript'}
+  {image: img1, name: 'Proiect1', category: 'HTML-CSS', details: 'Lorem ipsum...'}, 
+  {image: img2, name: 'Proiect2', category: 'Javascript', details: 'Lorem ipsum...'}, 
+  {image: img3, name: 'Proiect3', category: 'Javascript', details: 'Lorem ipsum...'}
 ]  
 
   const [items, setItems]= useState(defaultItems)  
 
-  const filteredItems =(filterBy) =>{ // change to filterItems
+  const filterItems =(filterBy) =>{ // change to filterItems
       let filteredList; //
 
       if (!filterBy){ //nu avem categorie
@@ -37,26 +37,57 @@ function Home() {
         <h1 className="quote">violets are blue,</h1>
         <h1 className="quote">unexpected {'{'}</h1>
         <h1 className="quote">on line 32</h1>
+    </div>
+
+       
+        <div className="scrolldown">
+        <span id="scroll"></span>
+        <span id="scroll"></span>
         </div>
 
-        <div className="buttonBox">
+{/*scriptul jquery pentru scrolldown*/}        
+{/*$(window).scroll(function(){     
+  if($(window).scrollTop() > 100){
+      $(".scroll").hide("slow");
+  }
+});
+
+$(window).scroll(function(){   
+  if($(window).scrollTop() < 100){
+      $(".scroll").show("slow");
+  }
+});
+*/}
+
+{/*document.getElementById('window').scroll(() => {
+   if(document.getElementById('window'))
+
+})*/}
+
+
+
+<div className="buttonBox">
         
-        <button onClick={()=>filteredItems('HTML-CSS')} className="filterButton">HTML, CSS</button>
+        <button onClick={()=>filterItems('HTML-CSS')} className="filterButton">HTML, CSS</button>
         
         {/*cu arrow function rulam functia filteredItems cand se face click, nu la incarcarea paginii */}
         
-        <button onClick={()=>filteredItems('Javascript')} className="filterButton">Javascript</button>
+        <button onClick={()=>filterItems('Javascript')} className="filterButton">Javascript</button>
 
-        <button onClick={()=>filteredItems() } className="filterButton">All</button>
-        </div>
+        <button onClick={()=>filterItems() } className="filterButton">All</button>
+</div>
        
 
-        <ul>
+
+        <ul className="listItems">
          {items.map((item)=>{ //afisam lista filtrata sau nefiltrata formatata cu html 
              return( <li>
-                 {item.name}
                  <img src={item.image} alt={item.name}/>
-
+                 <ul>
+                   <li className="itemName">{item.name}</li>
+                   <li className="itemDetails">{item.details}</li>
+                 </ul>
+                
              </li>)
          })}
 
